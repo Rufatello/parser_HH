3b525a2b3500a6f3c6bd8ce9123191e25f60f725
geydarovr <www.rufat@bk.ru>
2023-11-07 13:46:24 +0300

    start

--- progect/main.py
+++ progect/main.py
@@ -1,14 +1,15 @@
 import json
-from utils import get_load_company_url, create_database
+from utils import get_load_company_url, create_database, save_data_to_database
 from config import config
-
-companies = [1740]
+companies = [1740, 2180, 2748, 78638, 3148, 3529, 749819, 6041, 2227671, 740]
+# companies = [1740]
 def main():
 
     params = config()
-    data = get_load_company_url(companies)
+    data, url_company = get_load_company_url(companies)
     create_database('coursework', params)
-    print(data)
+    save_data_to_database(data, url_company, 'coursework', params)
+    print(url_company)
     with open('/home/geydarovr/Загрузки/db_course_work/1.json', 'w') as file:
         json.dump(data, file, ensure_ascii=False, indent=2)
 
