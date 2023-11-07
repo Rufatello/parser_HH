bff7e5a626d98ece51035aca3fa515bff85049c5
geydarovr <www.rufat@bk.ru>
2023-11-07 14:07:03 +0300

    Final

--- progect/main.py
+++ progect/main.py
@@ -1,6 +1,6 @@
-import json
 from utils import get_load_company_url, create_database, save_data_to_database
 from config import config
+from DB_manager import DBManager
 companies = [1740, 2180, 2748, 78638, 3148, 3529, 749819, 6041, 2227671, 740]
 # companies = [1740]
 def main():
@@ -9,9 +9,12 @@ def main():
     data, url_company = get_load_company_url(companies)
     create_database('coursework', params)
     save_data_to_database(data, url_company, 'coursework', params)
-    print(url_company)
-    with open('/home/geydarovr/Загрузки/db_course_work/1.json', 'w') as file:
-        json.dump(data, file, ensure_ascii=False, indent=2)
+    db_manager = DBManager('coursework')
+    print(db_manager.get_all_vacancies())
+    print(db_manager.get_avg_salary())
+    print(db_manager.get_vacancies_with_higher_salary())
+    print(db_manager.get_vacancies_with_keyword())
+    print(db_manager.get_companies_and_vacancies_count())
 
 if __name__=='__main__':
     main()
